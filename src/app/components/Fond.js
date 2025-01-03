8cde393cfa6a168f78f407f0dd7be335d1f5887a
Blankaert <blankaertlouis@outlook.com>
2025-01-03 23:44:00 +0100

    changement de photos

--- src/app/components/Fond.js
+++ src/app/components/Fond.js
@@ -4,8 +4,8 @@ export default function Fond() {
   return (
     <div className="relative">
       <img 
-          src='/images/city.png' // Chemin relatif à partir du dossier public
-          alt="cite japonaise"
+          src='/images/ongles.png' // Chemin relatif à partir du dossier public
+          alt="manucure"
           className='h-screen w-full object-cover' // Utilise object-cover pour éviter la déformation
       />
       <div className='absolute inset-0 bg-black opacity-50'></div> {/* Couche sombre */}
